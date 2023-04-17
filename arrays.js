6c20cc1411fd6b052fede005ff5cffa776835e4e
Your Name <kilasaniso@gmail.com>
2023-04-17 10:50:36 +0300

    first commit

---
+++ arrays.js
@@ -0,0 +1,20 @@
+//create an array type int called myNumbers.
+myNumbers =[30,23,12,34];
+console.log(myNumbers);
+
+//print the value of the second element in the myNumber array.
+myNumbers=[14,1,19,10];
+console.log(myNumbers[1]);
+
+//change value of first element to 33.
+myNumbers=[26,10,34,30,];
+myNumbers[0]=33;
+console.log(myNumbers);
+
+//loop through the element in the arrays using a for loop.
+myNumbers=[10,15,17,23,45];
+for (let i= 0; i< myNumbers.length; i++) {
+    console.log([i]);
+    
+}
+
